9ec247fc687028ecc4de8e54a6b80ccbb9dae113
Camila Andrade <camilaluanarodrigues9@gmail.com>
2024-02-10 20:49:25 -0300

    help

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ window.addEventListener("scroll", function(){
     let header = this.document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+/* MENU */
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
+
+function clickMenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
+
